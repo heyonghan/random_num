46d8c937e3fdd7141cc0a5074c386d9e4582260c
Gecko <heyonghan@hotmail.com>
2024-02-28 09:45:49 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import random
 st.title('随机数生成器')
 
 # 创建输入框让用户输入随机数的上限和下限
-lower_bound = st.number_input('输入随机数下限:', value=0.0)
-upper_bound = st.number_input('输入随机数上限:', value=100.0)
+lower_bound = st.number_input('输入随机数下限:', value=0.0, step=1.0)
+upper_bound = st.number_input('输入随机数上限:', value=100.0, step=1.0)
 
 # 创建一个按钮，当点击时生成随机数
 if st.button('生成随机数'):
